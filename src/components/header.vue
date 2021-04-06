957d0f46cd324346f0764310a83582b448a4d0a2
Chen1996-1 <2435740021@qq.comm>
2021-04-06 20:59:09 +0800

    修改header 的样式

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,7 +3,7 @@
     <div class="header-left">
       <span class="iconfont icon-back">&#xe601;</span>
     </div>
-    <div class="header-input">
+    <div class="header-center">
 
       <span class="iconfont icon-search">&#xe608;</span>
       输入城市/景点/主题
@@ -26,33 +26,29 @@ export default {
 <style lang='stylus' scoped>
   @import '~styles/varibles.styl'
   .header
+    background:$bgColor
     display: flex
-    line-height: .86rem
-    background: $bgColor
-    color: #fff
+    height: 2.5rem
+    line-height: 2.5rem
+
     .header-left
-      width: .64rem
-      float: left
+      margin-left: 1rem
+      margin-right: 1.5rem
       .icon-back
-        text-align: center
-        font-size: .4rem
-    .header-input
-      flex: 1
-      height: .64rem
-      line-height: .64rem
-      margin-top: .12rem
-      margin-left: .2rem
-      padding-left: .2rem
+        font-size: 1.3rem
+    .header-center
+      margin-right: 1rem
       background: #fff
-      border-radius: .1rem
+      border-radius: .25rem
+      margin-top: .15rem
+      margin-bottom: .25rem
+      padding-left: .8rem
+      padding-right:3rem
       color: #ccc
-      .icon-search
-        margin-left: .5rem
     .header-right
-      width: 1.24rem
-      float: right
       text-align: center
       .icon-downselect
-        font-size: .24rem
-        margiin-left: -.4rem
+        font-size: .8rem
+        padding-left: .2rem
+
 </style>
