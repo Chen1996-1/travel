4e94327a8df3cb0c2114a98f8b5e37f2a6678c91
Chen1996-1 <2435740021@qq.comm>
2021-04-06 20:23:03 +0800

    创建了一个header

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './header.vue'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont icon-back">&#xe601;</span>
+    </div>
+    <div class="header-input">
+
+      <span class="iconfont icon-search">&#xe608;</span>
+      输入城市/景点/主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-downselect">&#xe611;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  components: {
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .icon-back
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+      .icon-search
+        margin-left: .5rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .icon-downselect
+        font-size: .24rem
+        margiin-left: -.4rem
+</style>
