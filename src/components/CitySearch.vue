9360d27cd6dabb1eb74208917bb640785560b97e
Chen1996-1 <2435740021@qq.comm>
2021-04-18 10:54:16 +0800

    添加了city search组件

---
+++ src/components/CitySearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.search
+  height: 2.5rem
+  padding: 0 .5rem
+  background: $bgColor
+  .search-input
+    box-sizing: border-box
+    width: 100%
+    height: 2rem
+    line-height: 2rem
+    padding: 0 .5rem
+    text-align: center
+    border-radius: .3rem
+    color: #666
+</style>
