0ae3cd74a2c2e22217dd35b757095f1af9b070a3
Chen1996-1 <2435740021@qq.comm>
2021-05-03 08:47:37 +0800

    添加详情页的header

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,20 +1,26 @@
 <template>
  <div>
    <detail-banner></detail-banner>
+   <detail-header></detail-header>
+   <div class="test-height"></div>
  </div>
 </template>
 
 <script>
 import DetailBanner from './DetailBanner.vue'
+import DetailHeader from './DetailHeader.vue'
 
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+.test-height
+  height: 1000rem
 
 </style>
---
+++ src/components/DetailHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="absShow">
+      <span class="iconfont icon-back">&#xe601;</span>
+    </router-link>
+    <div class="header-fixed" v-show="fixedShow" :style="opacityStyle">
+      <router-link tag="div" to="/" class="fixed-back">
+        <span class="iconfont icon-back">&#xe601;</span>
+      </router-link>
+      详情页面
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      absShow: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  computed: {
+    fixedShow () {
+      let fixedShow = !this.absShow
+      return fixedShow
+    }
+  },
+  methods: {
+    handleScroll () {
+      console.log('Detailscrolling')
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {opacity}
+        this.absShow = false
+      } else {
+        this.absShow = true
+      }
+    }
+
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header-abs
+  position: absolute
+  left: 1.2rem
+  top: 1.2rem
+  width: 2.6rem
+  height: 2.6rem
+  border-radius: 1.3rem
+  background: red
+  text-align: center
+  line-height: 2.6rem
+  .icon-back
+    font-size: 1.3rem
+    color: #fff
+.header-fixed
+  position: fixed
+  top: 0
+  left: 0
+  right: 0
+  overflow: hidden
+  height: $headerHeight
+  line-height: $headerHeight
+  text-align: center
+  background: green
+  .fixed-back
+    position: absolute
+    left: 1rem
+    .icon-back
+      color: #fff
+      font-size: 1.2rem
+</style>
